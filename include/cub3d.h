b6e47145e24537f9439ada05771c3094bff20d95
Michelangelo Vassallo <mvassall@student.42madrid.com>
2025-09-25 18:30:08 +0200

    42

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvassall <mvassall@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user1 <user1@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 19:53:56 by mvassall          #+#    #+#             */
-/*   Updated: 2025/09/25 15:21:12 by mvassall         ###   ########.fr       */
+/*   Updated: 2025/09/25 18:28:08 by user1            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 #include "ctx.h"
+#include <stdint.h>
 
 # define FOV			66		// degrees
 # define WINDOW_WIDTH	1920	// pixels
@@ -34,6 +35,21 @@ typedef struct	s_fps_stats
 	int	samples[FPS_STATS_N_SAMPLES];
 	int	n;
 }	t_fps_stats;
+
+typedef struct	s_slice
+{
+	uint32_t	m;
+	uint32_t	na;
+	uint32_t	nb;
+	uint32_t	la;
+	uint32_t	lb;
+	uint32_t	wa;
+	uint32_t	wb;
+	uint32_t	s[4];
+	uint32_t	c[4];
+	uint32_t	i;
+	uint32_t	j;
+}	t_slice;
 
 int		run_app(t_ctx *ctx);
 void	cub3d_key_callback(mlx_key_data_t keydata, void *param);
--- src/cub3d/cub3d_4.c
+++ src/cub3d/cub3d_4.c
@@ -3,62 +3,100 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d_4.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvassall <mvassall@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user1 <user1@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 14:15:17 by mvassall          #+#    #+#             */
-/*   Updated: 2025/09/25 15:29:18 by mvassall         ###   ########.fr       */
+/*   Updated: 2025/09/25 18:29:00 by user1            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MLX42/MLX42.h"
 #include "cub3d.h"
+#include "libft.h"
 #include <stdint.h>
 
-static uint32_t	gcd(uint32_t a, uint32_t b)
-{
-	uint32_t	r;
-
-	r = a % b;
-	while (r != 0)
-	{
-		a = b;
-		b = r;
-		r = a % b;
-	}
-	return (b);
-}
-
-static void	calc_weights(uint32_t *a, uint32_t *b)
-{
-	uint32_t	mmc;
-	
-	mmc = ((uint64_t)(*a) * (uint64_t)(*b)) / gcd(*a,*b);
-	*a = mmc / *a;
-	*b = mmc / *b;
-}
-
-static void	get_txt_pix(mlx_texture_t *txt, uint32_t lin, uint32_t col, uint32_t *rgb)
+static void	get_txt_pix(mlx_texture_t *txt, uint32_t lin, uint32_t col, uint32_t *rgba)
 {
 	uint32_t	c;
 	
 	c = (txt->pixels[lin * txt->width + col]);
-	
+	rgba[0] = c & 0xff;
+	rgba[1] = (c >> 8) & 0xff;
+	rgba[2] = (c >> 16) & 0xff;
+	rgba[3] = (c >> 24) & 0xff;
 }
 
+static uint32_t	calculate_pix(uint32_t *rgba, uint32_t w)
+{
+	uint32_t	wh;
+	int			i;
 
-int	cub3d_slice_texture(mlx_texture_t *txt, uint32_t col,
+	i = -1;
+	wh = w / 2;
+	while (++i < 4)
+		rgba[i] = (rgba[i] + wh) / w;
+	return ((rgba[0] & 0xff) | ((rgba[1] & 0xff) << 8) | ((rgba[2] & 0xff) << 16)
+		| ((rgba[3] & 0xff) << 24));
+}
+
+static void	add(uint32_t *s, uint32_t *c)
+{
+	int	i;
+
+	i = -1;
+	while (++i < 4)
+		s[i] += c[i];
+}
+
+static void	init_p(uint32_t na, uint32_t nb, t_slice *p)
+{
+	uint64_t	mmc;
+	uint32_t	r;
+
+	ft_bzero(p, sizeof(t_slice));
+	p->na = na;
+	p->nb = nb;
+	mmc = ((uint64_t)(na) * (uint64_t)(nb));
+	r = na % nb;
+	while (r != 0)
+	{
+		na = nb;
+		nb = r;
+		r = na % nb;
+	}
+	p->m = mmc / nb;
+	p->la = p->m / p->na;
+	p->lb = p->m / p->nb;
+	p->wa = p->la;
+	p->wb = p->lb;
+}
+
+int	cub3d_slice_texture(mlx_texture_t *txt, uint32_t src_col,
 	uint32_t len, uint32_t *col_pixes)
 {
-	uint32_t	wa;
-	uint32_t	wb;
-	uint32_t	i;
-	uint32_t	j;
+	t_slice	p;
 
-	if (!txt || !col_pixes || col >= txt->width || len > txt->height)
+	if (txt == NULL || col_pixes == NULL || src_col >= txt->width
+		|| txt->height == 0 || len > txt->height || len == 0)
 		return (0);
-	wa = txt->height;
-	wb = len;
-	calc_weights(&wa, &wb);
-	
+	init_p(txt->height, len, &p);
+	get_txt_pix(txt, p.i, src_col, p.c);
+	while (p.j < len)
+	{
+		if (p.wa == 0)
+		{
+			p.wa = p.la;
+			get_txt_pix(txt, ++p.i, src_col, p.c);
+		}
+		if (p.wb == 0)
+		{
+			col_pixes[p.j++] = calculate_pix(p.s, p.lb);
+			ft_bzero(p.s, 4 * sizeof(uint32_t));
+			p.wb = p.lb;
+		}
+		add(p.s, p.c);
+		p.wa--;
+		p.wb--; 
+	}
 	return (1);
 }
